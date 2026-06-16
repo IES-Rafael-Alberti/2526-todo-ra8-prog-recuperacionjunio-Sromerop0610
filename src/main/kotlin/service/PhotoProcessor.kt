d896a36e81af471de58df19594dd2d57c01c332c
Sara Romero <sara.romerop256@gmail.com>
2026-06-16 13:27:40 +0200

    feat(service): terminado de implementar el procesado de fotos y tratado de su información

--- src/main/kotlin/service/PhotoProcessor.kt
+++ src/main/kotlin/service/PhotoProcessor.kt
@@ -1,55 +1,84 @@
 package org.iesra.service
 
 import org.iesra.model.PhotoFile
+import org.iesra.model.PhotoTypeStats
 import org.iesra.model.ProcessingResult
 import org.iesra.model.ReflexPhotoFile
+import org.iesra.model.RenameCommand
 import org.iesra.model.SmartphonePhotoFile
 import org.iesra.model.TripInput
 import kotlin.text.substringAfter
-
+//string.format(
 class PhotoProcessor {
     fun process(input: TripInput) : ProcessingResult{
-        var commands = mutableListOf<String>()
+        var commands = mutableListOf<RenameCommand>()
 
         var photosFile = buildValidSmartphonePhotos(input.namesSmartphone) + buildValidReflexPhotos(input.namesReflex)
         photosFile.sortedBy { it.orderKey }
 
         var index = 0
-        photosFile.forEach {
-            var command = input.destination + "00$index"
+        photosFile.forEach { name ->
+            var contador = String.format("%d3%", index)
+            var newName = input.destination + "_" + contador + ".jpg"
+            var command = "mv ${name.originalName} $newName"
+
+            commands.add(RenameCommand(command, newName))
+            index ++
         }
-        return ProcessingResult()
+
+        var procesadosSmart = buildValidSmartphonePhotos(input.namesSmartphone).size
+        var noProcesadosSmart = input.namesSmartphone.size - procesadosSmart
+
+        var procesadosReflex = buildValidReflexPhotos(input.namesSmartphone).size
+        var noProcesadosReflex = input.namesReflex.size - procesadosSmart
+
+        return ProcessingResult(
+            commands,
+            PhotoTypeStats(
+                buildValidReflexPhotos(input.namesSmartphone).size,
+                procesadosReflex,
+                noProcesadosReflex
+            ),
+            PhotoTypeStats(
+                buildValidSmartphonePhotos(input.namesSmartphone).size,
+                procesadosSmart,
+                noProcesadosSmart
+            ),
+            procesadosSmart + procesadosReflex
+        )
     }
 
-    fun buildValidSmartphonePhotos(names: List<String>): List<PhotoFile> {
-        var smartPhotos = mutableListOf<PhotoFile>()
-        names.forEach { name ->
-            var partes = name.substringAfter("IMG").split("_")
-            var orderKey = partes[0] + partes[1]
-            smartPhotos.add(PhotoFile(
-                name,
-                orderKey
-            ))
-        }
+    fun buildValidSmartphonePhotos(namesSmartphone: List<String>): List<SmartphonePhotoFile> {
+        var photos = mutableListOf<SmartphonePhotoFile>()
+        namesSmartphone.forEach { name ->
+            var parserName = name.substringAfter("IMG").substringBefore(".jpg")
+            var parts = parserName.split("_")
+            var contName = listOf(parserName)
+            var parseAnioMesDia = listOf(contName[1])
+            var parseHourMinuteSeconds = listOf(contName[3])
+            var originalName = contName[1] + contName[3]
 
-        return smartPhotos
+            if (name.startsWith("IMG") && name.endsWith(".jpg") && parts.size == 4 && parts[0] == "_" && parts[2] == "_" && parseAnioMesDia.size == 8 && parseHourMinuteSeconds.size == 6) {
+                photos.add(SmartphonePhotoFile(name))
+            }
+        }
+        return photos
     }
 
-    fun buildValidReflexPhotos(names: List<String>): List<PhotoFile> {
-        var reflexPhotos = mutableListOf<PhotoFile>()
+    fun buildValidReflexPhotos(namesSmartphone: List<String>): List<ReflexPhotoFile> {
+        var photos = mutableListOf<ReflexPhotoFile>()
+        namesSmartphone.forEach { name ->
+            var parserName = name.substringAfter("P").substringBefore(".jpg")
+            var parts = parserName.split("_")
+            var contName = listOf(parserName)
+            var parseAnioMesDia = listOf(contName[0])
+            var parseHourMinuteSeconds = listOf(contName[2])
+            var originalName = contName[0] + contName[2]
 
-        names.forEach { name ->
-            var partes = name.substringAfter("P").split("_")
-            var anioMesDia = listOf(partes[0])
-            var anio = "20" + anioMesDia[4] + anioMesDia[5]
-            var mes = anioMesDia[2] + anioMesDia[3]
-            var dia = anioMesDia[1] + anioMesDia[2]
-
-            var orderKey = anio+mes+dia+partes[2]
-            reflexPhotos.add(PhotoFile(
-                name,
-                orderKey
-            ))
+            if (name.startsWith("IMG") && name.endsWith(".jpg") && parts.size == 3 && parts[1] == "_" && parseAnioMesDia.size == 8 && parseHourMinuteSeconds.size == 6) {
+                photos.add(ReflexPhotoFile(name))
+            }
         }
+        return photos
     }
 }
