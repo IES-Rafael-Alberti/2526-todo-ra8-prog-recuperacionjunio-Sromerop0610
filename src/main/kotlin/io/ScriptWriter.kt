04d5e781b2e39b271a6a5a7bbdf84799aabef333
Sara Romero <sara.romerop256@gmail.com>
2026-06-16 13:28:06 +0200

    feat(io): implementada la escritura del archivo sh

--- src/main/kotlin/io/ScriptWriter.kt
+++ src/main/kotlin/io/ScriptWriter.kt
@@ -1,24 +1,42 @@
 package org.iesra.io
 
 import org.iesra.model.PhotoFile
+import org.iesra.model.ProcessingResult
+import org.iesra.model.RenameCommand
 import org.iesra.model.TripInput
 import java.nio.file.Files
 import java.nio.file.Path
 
 class ScriptWriter {
     fun write(
-        tripInput: TripInput,
-        photoFile: List<PhotoFile>,
-        outPutRoute: Path
+        outPutRoute: Path,
+        place: String,
+        comandos: List<RenameCommand>
     ) {
-        var outPutFile = outPutRoute.resolve("$tripInput.destination.sh")
+        var outPutFile = outPutRoute.resolve("$place.sh")
 
-        var lines = buildList {
-            photoFile.forEach { image ->
-                add("mv ${image.originalName} ${image.orderKey}")
-            }
-
+        comandos.forEach {
+            file.write(it.toScriptLine())
         }
         Files.write(outPutFile, lines)
     }
 }
+
+/*
+fun write(
+        outPutRoute: Path,
+        place: String,
+        comandos: List<RenameCommand>
+    ) {
+        var outPutFile = outPutRoute.resolve("$place.sh")
+
+        var lines = buildList {
+            comandos.forEach {
+                add(it.toScriptLine())
+            }
+        }
+
+        Files.write(outPutFile, lines)
+    }
+}
+ */
