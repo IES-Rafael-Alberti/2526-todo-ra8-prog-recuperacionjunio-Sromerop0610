52db89c365b9eaba7bf035eddeabb00c6fd9c3ab
Sara Romero <sara.romerop256@gmail.com>
2026-06-16 09:36:12 +0200

    feat(io): implementación del metodo de escritura del fichero de salida .sh

---
+++ src/main/kotlin/io/ScriptWriter.kt
@@ -0,0 +1,24 @@
+package org.iesra.io
+
+import org.iesra.model.PhotoFile
+import org.iesra.model.TripInput
+import java.nio.file.Files
+import java.nio.file.Path
+
+class ScriptWriter {
+    fun write(
+        tripInput: TripInput,
+        photoFile: List<PhotoFile>,
+        outPutRoute: Path
+    ) {
+        var outPutFile = outPutRoute.resolve("$tripInput.destination.sh")
+
+        var lines = buildList {
+            photoFile.forEach { image ->
+                add("mv ${image.originalName} ${image.newName}")
+            }
+
+        }
+        Files.write(outPutFile, lines)
+    }
+}
